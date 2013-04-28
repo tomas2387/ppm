c900a0141715332264c97c00da5b55f0c989baa1
Tomas Prado <tomas.prado@eyeos.com>
2013-04-28 12:58:09 +0200

    Base rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppm::Application.config.secret_token = '08eb50ca6849c46429376e6b955a402630e9a611d399dc9eb70a655e91b2766aa0469abf43b57e00f6034fa41e2dbeab1f6d6165d83063f07a78be5b417647f1'

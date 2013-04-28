b8202b3a9ad169b98e26aba5e2570008ee54ad77
tomas2387 <tomas2387@gmail.com>
2013-04-28 22:56:55 +0200

    Create, show and list all methods created in the player controller

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,5 +1,21 @@
 class PlayerController < ApplicationController
   def index
-    Player.all
+    @players = Player.all
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def new
+  end
+
+  def create
+    player = Player.new(:name => params[:name])
+    player.save
+    redirect_to('/')
+  end
+
+  def show
+    @player = Player.find(params[:id])
   end
 end

0c06c7c7d285435fa4653172328f254d872806bf
tomas2387 <tomas2387@gmail.com>
2013-04-29 23:07:03 +0200

    Show basic statistics for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :name
-  has_many 'Game'
+  has_many :local_games, :class_name => 'Game', :foreign_key => 'local_player_id'
+  has_many :away_games, :class_name => 'Game', :foreign_key => 'away_player_id'
 
   validates :name,  :presence => true
 end
